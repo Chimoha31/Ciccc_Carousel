18b4fd20e2e63dfe8b15cbb15965fe8ea4fb43f7
Chimoha31 <mokochii1108@gmail.com>
2022-01-27 17:07:01 -0800

    DONE connect to Button color to click

--- main.js
+++ main.js
@@ -3,9 +3,11 @@
 const prevBtn = document.querySelector("#prev-button");
 const nextBtn = document.querySelector("#next-button");
 const imgFrame = document.querySelector(".imageCarousel");
+const nav = document.querySelector("nav");
 let imgTag = document.querySelector("img");
-
 let currentIndex = 0;
+
+const dots = [];
 
 const images = [
   "./images/avocado.jpeg",
@@ -15,7 +17,6 @@ const images = [
 ];
 
 nextBtn.addEventListener("click", function () {
-  // console.log(currentIndex);
   if (currentIndex === images.length - 1) {
     currentIndex = 0;
     imgTag.setAttribute("src", "");
@@ -23,18 +24,43 @@ nextBtn.addEventListener("click", function () {
   } else {
     currentIndex++;
     imgTag.setAttribute("src", images[currentIndex]);
+
+    for (let j = 0; j < images.length; j++) {
+      currentIndex = j;
+      dots.forEach((dot) => {
+        dot.classList.remove("current");
+      });
+      dots[currentIndex].classList.add("current");
+    }
   }
 });
 
-
 prevBtn.addEventListener("click", function () {
   console.log(currentIndex);
-  if(currentIndex === 0) {
+  if (currentIndex === 0) {
     currentIndex = images.length - 1;
-    imgTag.setAttribute('src', images[currentIndex]);
-  }else {
+    imgTag.setAttribute("src", images[currentIndex]);
+  } else {
     currentIndex--;
     imgTag.setAttribute("src", images[currentIndex]);
   }
 });
 
+// dot
+
+for (let i = 0; i < images.length; i++) {
+  let btnDot = document.createElement("button");
+  btnDot.addEventListener("click", function () {
+    currentIndex = i;
+    dots.forEach((dot) => {
+      dot.classList.remove("current");
+    });
+    dots[currentIndex].classList.add("current");
+    // bottonを押したらそのボタンの順番通りの画像を抽出
+    imgTag.setAttribute("src", images[currentIndex]);
+  });
+  dots.push(btnDot);
+  nav.appendChild(btnDot);
+  // 最初の画像には(アボカドの画像)最初からdotに色を付与
+  dots[0].classList.add("current");
+}
